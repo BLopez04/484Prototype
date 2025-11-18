233158e3d6ed9adaf35f42b91dd6def5bdc8c061
Ian Wong <iwong12@calpoly.edu>
2025-11-18 00:18:54 -0800

    working toasts

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { TaskProvider } from './contexts/TaskContext.tsx'
+import { ToastProvider } from './contexts/ToastContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TaskProvider>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </TaskProvider>
   </StrictMode>,
 )
